2ab78f0adbf16c81bc7584e74e4675751364f19b
TiN_redmc <thipok17@hotmail.com>
2017-11-26 10:46:42 +0700

    Add files via upload
    removed day of weeks .because not working.

--- HPDL1414_clock.ino
+++ HPDL1414_clock.ino
@@ -17,10 +17,10 @@ RTC_DS1307 rtc = RTC_DS1307();
 
 int hours = 0;
 int minutes = 0;
-char dow;
 int yea = 0;
 int mon = 0;
 int days = 0;
+
 void setup() {
  Serial.begin(9600);
   rtc.begin();
@@ -36,39 +36,29 @@ void loop() {
     // Now set the hours and minutes.
     hours = now.hour();
     minutes = now.minute();
-    dow = now.dayOfTheWeek();
     days = now.day();
     mon = now.month();
     yea = now.year();
-int displayValue = hours*100 + minutes;
-int dayDisplay = days*1;
-int monthDispaly = mon*1;
-int yearDisplay = yea*1;
+    
+unsigned int displayValue = hours*100 + minutes;
 String str= String(displayValue);
-String dowS = String(dow);
 String dayS= String(days);
 String monS= String(mon);
 String yeaS= String(yea);
+
   // Now print the time value to the display.
-  dl1414.writeStringFix(str);
+ 
+  dl1414.writeStringScrolling(str, 120);
   Serial.print(str);
-  Serial.print("/");
-  delay(500);
-  dl1414.writeStringFix(dowS);
-  Serial.print(dowS);
-  Serial.print("/");
-  delay(500);
-  dl1414.writeStringFix(dayS);
+  Serial.println("/");
+  dl1414.writeStringScrolling(dayS, 120);
   Serial.print(dayS);
-  Serial.print("/");
-  delay(500);
-  dl1414.writeStringFix(monS);
+  Serial.println("/");
+  dl1414.writeStringScrolling(monS, 120);
   Serial.print(monS);
-  Serial.print("/");
-  delay(500);
-  dl1414.writeStringFix(yeaS);
+  Serial.println("/");
+  dl1414.writeStringScrolling(yeaS, 120);
   Serial.print(yeaS);
-  Serial.print("/");
-  delay(500);
+  Serial.println("/");
 }
 
